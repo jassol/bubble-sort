a7bf68b039d8331e12ebc54f523beeaca6bf3e0b
Ayse Erduran <ae2456@columbia.edu>
2020-02-18 12:22:36 -0500

    refactoactored solution to have a separate swap function

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,28 @@
+// // function bubbleSort(arr){
+//   let sorted = false;
+//   let swaps;
+//   while(!sorted){
+//     swaps = 0;
+//     for(let i=0; i< arr.length-1; i++){
+//       if(arr[i] > arr[i+1]){
+//         let temp = arr[i];
+//         arr[i] = arr[i+1];
+//         arr[i+1] = temp;
+//         swaps++
+//       }
+//     }
+//     if(swaps === 0){
+//       sorted = true;
+//     }
+//   }
+//   return arr;
+// }
+
+//refactored solution
+function swap(a, b){
+  return [b, a]
+}
+
 function bubbleSort(arr){
   let sorted = false;
   let swaps;
@@ -5,15 +30,14 @@ function bubbleSort(arr){
     swaps = 0;
     for(let i=0; i< arr.length-1; i++){
       if(arr[i] > arr[i+1]){
-        let temp = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = temp;
-        swaps++
+        let swapped = swap(arr[i], arr[i+1])
+        arr.splice(i, 2, swapped[0], swapped[1])
+        swaps++;
       }
     }
-    if(swaps === 0){
-      sorted = true;
-    }
+    if (!swaps) sorted = true;
   }
   return arr;
 }
+
+
