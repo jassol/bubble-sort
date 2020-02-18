2fb5d65de97919b6b424aac0d0f9d2c30884ebe6
Ayse Erduran <ae2456@columbia.edu>
2020-02-18 15:23:32 -0500

    added split function for merge sort

--- bubblesort.js
+++ bubblesort.js
@@ -19,25 +19,26 @@
 // }
 
 //refactored solution
-function swap(a, b){
-  return [b, a]
-}
+// function swap(a, b){
+//   return [b, a]
+// }
 
-function bubbleSort(arr){
-  let sorted = false;
-  let swaps;
-  while(!sorted){
-    swaps = 0;
-    for(let i=0; i< arr.length-1; i++){
-      if(arr[i] > arr[i+1]){
-        let swapped = swap(arr[i], arr[i+1])
-        arr.splice(i, 2, swapped[0], swapped[1])
-        swaps++;
-      }
-    }
-    if (!swaps) sorted = true;
-  }
-  return arr;
-}
+// function bubbleSort(arr){
+//   let sorted = false;
+//   let swaps;
+//   while(!sorted){
+//     swaps = 0;
+//     for(let i=0; i< arr.length-1; i++){
+//       if(arr[i] > arr[i+1]){
+//         let swapped = swap(arr[i], arr[i+1])
+//         arr.splice(i, 2, swapped[0], swapped[1])
+//         swaps++;
+//       }
+//     }
+//     if (!swaps) sorted = true;
+//   }
+//   return arr;
+// }
+
 
 
